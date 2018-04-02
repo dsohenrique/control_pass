e261aa581cc50cd20cb4a6d2d6305d89639b5f43
Douglas <dsohenrique>
2018-04-02 20:42:17 -0300

    Insere classes

--- src/main/java/br/com/controlpass/model/Aluno.java
+++ src/main/java/br/com/controlpass/model/Aluno.java
@@ -1,131 +1,39 @@
-package br.com.controlpass.model;
-
 import java.util.Date;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.PrePersist;
-import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
-//import br.com.controlpass.enums.CursoEnum;
-//import br.com.controlpass.enums.ModuloEnum;
 @Getter
 @Setter
-@Entity
-@Table(name = "TBL_ALUNO")
 public class Aluno {
-
-    @Id
-    @GeneratedValue
-    @Column(name = "ID_ALUNO")
-    private Long id;
-    private String aluno;
-    @Column(name = "NM_ALUNO")
-    private String nome;
-    @Column(name = "TX_CPF")
-    private Long cpf;
-    @Column(name = "TX_FONE")
-    private String telefone;
-    @Column(name = "TX_EMAIL")
-    private String email;
-    private Date nascimento;
-    /* mudar de String para Date*/
-    private String ano;
-    private String curso;
-    //private CursoEnum curso;
-    private String modulo;
-    //private ModuloEnum curso;
-    private String disciplina;
-    @Column(name = "DT_CRIACAO")
-    private Date dataCriacao;
-    @OneToOne(mappedBy = "aluno", cascade = CascadeType.ALL)
-    private br.com.controlpass.model.Endereco endereco = new br.com.controlpass.model.Endereco();
-
-    @PrePersist
-    private void preencherDataCriacao() {
-        if (dataCriacao == null) {
-            dataCriacao = new Date();
-        }
-    }
-    
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getNascimento() {
-        return nascimento;
-    }
-
-    public void setNascimento(Date nascimento) {
-        this.nascimento = nascimento;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    
-
-    public String getAno() {
-        return ano;
-    }
-
-    public void setAno(String ano) {
-        this.ano = ano;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
-
-    public String getModulo() {
-        return modulo;
-    }
-
-    public void setModulo(String modulo) {
-        this.modulo = modulo;
-    }
-
-    public String getDisciplina() {
-        return disciplina;
-    }
-
-    public void setDisciplina(String disciplina) {
-        this.disciplina = disciplina;
-    }
-
-    public Object getEndereco() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void setCpf(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private Date nascimento; /* mudar de String para Date*/
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+	
 }
--- src/main/java/br/com/controlpass/view/AlunoMB.java
+++ src/main/java/br/com/controlpass/view/AlunoMB.java
@@ -1,8 +1,7 @@
-package br.com.controlpass.view;
+package br.com.etechoracio.jpa.view;
 
-import br.com.controlpass.model.Aluno;
-import java.util.ArrayList;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -11,111 +10,53 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+
 import lombok.Getter;
 import lombok.Setter;
+import br.com.etechoracio.jpa.model.Aluno;
 
-@ManagedBean
 @Getter
 @Setter
+@ManagedBean
 public class AlunoMB {
 
-    private String cpf = "";
+	private Aluno edit = new Aluno();
+	private List<Aluno> registros;
 
-    public String consultar() {
-        if (cpf.equals("12345")) {
-            //return FacesContext.getCurrentInstance().getExternalContext().redirect("DadosALuno.xhtml");
-            return "pagina?faces-redirect=true";
-        } else {
-            return "CPF inv�lido";
-        }
-    }
-    private Aluno edit = new Aluno();
-    private List<Aluno> registros;
+	@PostConstruct
+	public void postConstruct() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("ETEC");
+		EntityManager em = emf.createEntityManager();
+		String sql = "SELECT a FROM Aluno a";
+		TypedQuery<Aluno> query = em.createQuery(sql, Aluno.class);
+		setRegistros(query.getResultList());
+	}
 
-    @PostConstruct
-    public void postConstruct() {
-        EntityManagerFactory emf = Persistence
-                .createEntityManagerFactory("ETEC");
-        EntityManager em = emf.createEntityManager();
-        String sql = "SELECT a FROM Aluno a";
-        TypedQuery<Aluno> query = em.createQuery(sql, Aluno.class);
-        registros = query.getResultList();
-    }
+	public void doSave() {
+		try {
+			Thread.sleep(8000);
+			edit.getEndereco().setAluno(edit);
 
-    public void doSave() {
-        try {
-            Thread.sleep(8000);
-            edit.getEndereco();
-           
-                   
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("ETEC");
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.persist(edit);
+			em.getTransaction().commit();
+			em.close();
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Regsitro inserido com sucesso.", null));
+			edit = new Aluno();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-            EntityManagerFactory emf = Persistence
-                    .createEntityManagerFactory("ETEC");
-            EntityManager em = emf.createEntityManager();
-            em.getTransaction().begin();
-            em.persist(edit);
-            em.getTransaction().commit();
-            em.close();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Regsitro inserido com sucesso.",
-                    null));
-            edit = new Aluno();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+	public List<Aluno> getRegistros() {
+		return registros;
+	}
 
-    private List<Aluno> alunos = new ArrayList<Aluno>();
-    // private List<Presenca> presencas = new ArrayList<Presenca>();
-    private Aluno exibicao = new Aluno();
+	public void setRegistros(List<Aluno> registros) {
+		this.registros = registros;
+	}
 
-    // private Presenca exibicao1 = new Presenca();
-
-   
-    
-
-    private void findAll() {
-        Aluno alu1 = new Aluno();
-        //alu1.setNome("Lucas");
-        alu1.setCpf("12345");
-        alu1.setAno("2018");
-        /*alu1.setCurso(CursoEnum.Selecione);
-		alu1.setCurso(CursoEnum.INFORM�TICA);
-		alu1.setCurso(CursoEnum.ADMINISTRA��O);
-		alu1.setCurso(CursoEnum.ELETR�NICA);
-		alu1.setCurso(CursoEnum.REDES);
-		alu1.setCurso(CursoEnum.MECATR�NICA);
-		alu1.setModulo(ModuloEnum.Selecione);
-		alu1.setModulo(ModuloEnum.PRIMEIRO);
-		alu1.setModulo(ModuloEnum.SEGUNDO);
-		alu1.setModulo(ModuloEnum.TERCEIRO);*/
-        alu1.setCurso("Selecione");
-        alu1.setCurso("INFORMATICA");
-        alu1.setCurso("ADMINISTRACAO");
-        alu1.setCurso("ELETRONICA");
-        alu1.setCurso("REDES");
-        alu1.setCurso("MECATRONICA");
-        alu1.setModulo("Selecione");
-        alu1.setModulo("PRIMEIRO");
-        alu1.setModulo("SEGUNDO");
-        alu1.setModulo("TERCEIRO");
-        alu1.setDisciplina("T�CNICAS PROGRAMA��O INTERNET II");
-
-        /*
-		 * Presenca alu2 = new Presenca(); alu2.setNome("Lucas");
-		 * alu2.setData("05/03/2018"); alu2.setEntrada("19:00");
-		 * alu2.setSaida("20:50"); alu2.setSituacao("Presente");
-         */
-        alunos.add(alu1);
-        // presencas.add(alu2);
-    }
-
-    public void doView(Aluno alu) {
-        // Busca no banco de dados pelo ID
-        exibicao = alu;
-    }
-
-    // public void verificarCpf(Aluno cpf){
-    // }
-    // }
 }
