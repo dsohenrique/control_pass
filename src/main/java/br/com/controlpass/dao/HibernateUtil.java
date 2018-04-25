b34d8cc6a9e4f7fe2a3742e76609233adbd60265
Douglas <dsohenrique@gmail.com>
2018-04-24 21:57:27 -0300

    Adiciona configuração e classes mapeadas hibernate

--- src/main/java/br/com/controlpass/dao/HibernateUtil.java
+++ src/main/java/br/com/controlpass/dao/HibernateUtil.java
@@ -1,34 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.com.controlpass.dao;
 
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author Dôga e Jeny
+ */
 public class HibernateUtil {
 
-    private static SessionFactory factory;
-    private static Configuration configuration;
-
-    public static SessionFactory initialize() {
+    private static final SessionFactory sessionFactory;
+    
+    static {
         try {
-            configuration = new Configuration().configure();
-
-            ServiceRegistry registro = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-
-            factory = configuration.buildSessionFactory(registro);
-
-            System.out.println(">> HIBERNATE INICIADO COM SUCESSO.");
-        } catch (Throwable e) {
-            System.out.println(">> FALHA NA INICIAÇÃO DO HIBERNATE.");
-            e.printStackTrace();
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
         }
-
-        return factory;
     }
-
-    public static SessionFactory getFactory() {
-        return factory;
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
-
 }
